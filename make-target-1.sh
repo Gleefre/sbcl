0b584ab2d1b3171062890b96882094227f1863df
Gleefre <varedif.a.s@gmail.com>
2023-04-18 22:49:07 +0200

    Move adb_run function back to make-target-1.sh, fix spaces

--- make-target-1.sh
+++ make-target-1.sh
@@ -22,7 +22,6 @@ export LANG LC_ALL
 
 # Load our build configuration
 . output/build-config
-. ./adb-run.sh
 
 if [ -n "$SBCL_HOST_LOCATION" ]; then
     echo //copying host-1 output files to target
@@ -40,16 +39,23 @@ echo //building runtime system and symbol table file
 $GNUMAKE -C src/runtime clean
 $GNUMAKE $SBCL_MAKE_JOBS -C src/runtime all
 
+android_run_for_output() {
+    adb push $1 /data/local/tmp/temp.out > /dev/null
+    adb shell chmod +x /data/local/tmp/temp.out > /dev/null
+    adb shell ./data/local/tmp/temp.out
+    adb shell rm /data/local/tmp/temp.out > /dev/null
+}
+
 # Use a little C program to grab stuff from the C header files and
 # smash it into Lisp source code.
 # -C tools-for-build is broken on some gnu make versions.
 if $android
 then
-	( cd tools-for-build; $CC -I../src/runtime -ldl -o grovel-headers grovel-headers.c)
-	adb_run tools-for-build/grovel-headers > output/stuff-groveled-from-headers.lisp
+    ( cd tools-for-build; $CC -I../src/runtime -ldl -o grovel-headers grovel-headers.c)
+    adb_run tools-for-build/grovel-headers > output/stuff-groveled-from-headers.lisp
 else
-	( cd tools-for-build; $GNUMAKE -I../src/runtime grovel-headers )
-	tools-for-build/grovel-headers > output/stuff-groveled-from-headers.lisp
+    ( cd tools-for-build; $GNUMAKE -I../src/runtime grovel-headers )
+    tools-for-build/grovel-headers > output/stuff-groveled-from-headers.lisp
 fi
 touch -r tools-for-build/grovel-headers.c output/stuff-groveled-from-headers.lisp
 
