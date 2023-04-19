1d62866a307cfb71f00e8a8843f6b89b3a72e214
Gleefre <varedif.a.s@gmail.com>
2023-04-19 19:38:14 +0200

    Don't blindly build all contribs

--- make-target-contrib-android.sh
+++ make-target-contrib-android.sh
@@ -50,16 +50,22 @@ build_system sb-introspect
 build_system sb-cltl2
 build_system sb-aclrepl
 build_system sb-sprof
-build_system sb-capstone
+if [ -f android-libs/libcapstone.so ]; then
+    build_system sb-capstone
+fi
 build_system sb-rotate-byte
 build_system sb-md5 sb-rotate-byte
 build_system sb-executable
-build_system sb-gmp
-build_system sb-mpfr sb-gmp
+if [ -f android-libs/libgmp.so ]; then
+    build_system sb-gmp
+    if [ -f android-libs/libmpfr.so ]; then
+        build_system sb-mpfr sb-gmp
+    fi
+fi
 build_system sb-concurrency
 build_system sb-queue sb-concurrency
 build_system sb-rt
 build_system sb-simple-streams sb-posix sb-bsd-sockets
 build_system sb-cover sb-md5
-build_system sb-simd
+# build_system sb-simd
 build_system sb-grovel asdf
