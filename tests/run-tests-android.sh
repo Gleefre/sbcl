7169d333171bdbe9796771e930a90bf84e505018
Gleefre <varedif.a.s@gmail.com>
2025-05-26 12:58:54 +0200

    Use SBCL_ANDROID_CROSS to indicate that we are crossrunning tests on android

--- tests/run-tests-android.sh
+++ tests/run-tests-android.sh
@@ -73,8 +73,8 @@ make_reloc_test() {
     fi
 }
 
-echo "adb shell \"(cd /data/local/tmp/sbcl/tests; LD_LIBRARY_PATH=/data/local/tmp/sbcl/android-libs ./run-tests.sh $@)\""
-adb shell "(cd /data/local/tmp/sbcl/tests; LD_LIBRARY_PATH=/data/local/tmp/sbcl/android-libs ./run-tests.sh $@)" 2>&1 | \
+echo "adb shell \"(cd /data/local/tmp/sbcl/tests; LD_LIBRARY_PATH=/data/local/tmp/sbcl/android-libs SBCL_ANDROID_CROSS=true ./run-tests.sh $@)\""
+adb shell "(cd /data/local/tmp/sbcl/tests; LD_LIBRARY_PATH=/data/local/tmp/sbcl/android-libs SBCL_ANDROID_CROSS=true ./run-tests.sh $@)" 2>&1 | \
     while read line; do
         line=$(echo "$line" | sed 's/\r//g')  # On older android line terminates with \r
         echo "$line" ;
