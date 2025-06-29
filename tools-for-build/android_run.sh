d404a59779e01a637eec193281d782e81044f217
Gleefre <varedif.a.s@gmail.com>
2025-06-29 09:39:24 +0200

    Remove \r from adb output in more places

--- tools-for-build/android_run.sh
+++ tools-for-build/android_run.sh
@@ -1,13 +1,13 @@
 android_run() {
     adb push "$1" "$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/temp.out" > /dev/null 2>&1
     adb shell "chmod +x \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/temp.out\"" > /dev/null 2>&1
-    adb shell "LD_LIBRARY_PATH=\"$SBCL_ANDROID_TARGET_LOCATION/output/android-libs\" \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/temp.out\" 2>/dev/null"
+    (adb shell "LD_LIBRARY_PATH=\"$SBCL_ANDROID_TARGET_LOCATION/output/android-libs\" \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/temp.out\" 2>/dev/null") | sed 's/\r//g'
     adb shell "rm \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/temp.out\"" > /dev/null 2>&1
 }
 
 android_run_for_exit_code() {
     adb push "$1" "$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/$1" > /dev/null 2>&1
     adb shell "chmod +x \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/$1\"" > /dev/null 2>&1
-    adb shell "echo input | LD_LIBRARY_PATH=\"$SBCL_ANDROID_TARGET_LOCATION/output/android-libs\" \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/$1\" > /dev/null 2>&1 ; echo \"\$?\"" 2>/dev/null
+    (adb shell "echo input | LD_LIBRARY_PATH=\"$SBCL_ANDROID_TARGET_LOCATION/output/android-libs\" \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/$1\" > /dev/null 2>&1 ; echo \"\$?\"" 2>/dev/null) | sed 's/\r//g'
     adb shell "rm \"$SBCL_ANDROID_TARGET_LOCATION/tools-for-build/$1\"" > /dev/null 2>&1
 }
